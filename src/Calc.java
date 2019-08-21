edf14d24fef5c56466ebb375d181d427ce17d9fa
Danil <lilkit1996@gmail.com>
2019-08-21 17:32:56 +0300

    1

--- src/Calc.java
+++ src/Calc.java
@@ -1,61 +1,71 @@
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calc {
 
 
+
+
     public void logic() {
+   try {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Введите число a");
+       int a = scanner.nextInt();
 
-        System.out.println("Введите математический знак операции");
-        Scanner scanner = new Scanner(System.in);
-        String sig = scanner.nextLine();
+       System.out.println("Введите число b");
+       int b = scanner.nextInt();
+       scanner.nextLine();
 
+       System.out.println("Введите математический знак операции");
+       String sig = scanner.nextLine();
 
+       Arifmetic arifmetic = new Arifmetic();
 
-        System.out.println("Введите число a");
-        int a = scanner.nextInt();
-
-        System.out.println("Введите число b");
-        int b = scanner.nextInt();
-
-        Arifmetic arifmetic = new Arifmetic();
-
-        switch (sig) {
-
-
-            case "+": {
-                int c = arifmetic.summa(a,b);
-                System.out.println("Результат " + c);
-                break;
-            }
+       switch (sig) {
 
 
-            case "-": {
-                int c = arifmetic.minus(a,b);
-                System.out.println("Результат " + c);
-                break;
-            }
+           case "+": {
+               int c = arifmetic.summa(a, b);
+               System.out.println("Результат " + c);
+               break;
+           }
 
-            case "*": {
-                int c = arifmetic.umnog(a,b);
-                System.out.println("Результат " + c);
-                break;
-            }
 
-            case "/": {
-                int c = arifmetic.delen(a,b);
-                System.out.println("Результат " + c);
-                break;
-            }
-            case "%": {
-                int c = arifmetic.ostacha(a,b);
-                System.out.println("Результат " + c);
-                break;
-            }
+           case "-": {
+               int c = arifmetic.minus(a, b);
+               System.out.println("Результат " + c);
+               break;
+           }
 
-            default:
-                System.out.println("Введите правильный математический знак операции ( + , - , * , /,%)");
-                logic();
-                break;
+           case "*": {
+               int c = arifmetic.umnog(a, b);
+               System.out.println("Результат " + c);
+               break;
+           }
+
+           case "/": {
+               int c = arifmetic.delen(a, b);
+               System.out.println("Результат " + c);
+               break;
+           }
+           case "%": {
+               int c = arifmetic.ostacha(a, b);
+               System.out.println("Результат " + c);
+               break;
+           }
+
+           default:
+               System.out.println("Введите правильный математический знак операции ( + , - , * , /,%)");
+               logic();
+               break;
+       }
+   }catch (InputMismatchException e) {
+       System.out.println("Неправильно введен параметр");
+       logic();
+   }
+
+
+
         }
     }
-}
